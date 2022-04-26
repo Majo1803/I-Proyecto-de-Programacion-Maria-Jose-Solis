b7bc4a88f401c487d2fed860d71f5b1cee10eafc
Majo1803 <marijosolis2@gmail.com>
2022-04-26 02:25:41 -0600

    comentario

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,7 @@
 from hashlib import md5
 from getpass import getpass
 
-
+#-------------------Funciones vistas en clase para obtener y cifrar claves----------------------------------------
 def cifrar (entrada):
     entrada_binaria=entrada.encode('ascii')
     resultado = md5(entrada_binaria)
